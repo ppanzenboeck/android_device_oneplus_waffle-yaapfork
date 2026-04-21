6b4ea3d232be9ad0dd592ae3058e53c56c28a4d7
ppanzenboeck <panz.development@gmail.com>
2026-04-21 13:39:51 +0200

    Revert "waffle: Update from OOS 11.F.43"
    This reverts commit bfba4e682a15a254ea1db300916915f0bec80c81.

--- lineage_waffle.mk
+++ lineage_waffle.mk
@@ -23,8 +23,8 @@ PRODUCT_MODEL := CPH2573
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="qssi_64-user 16 BP2A.250605.015 1773717273328 release-keys" \
-    BuildFingerprint=OnePlus/CPH2573IN/OP595DL1:16/BP2A.250605.015/U.R4T3.37c1ac7-14b7cd5-14d96f8:user/release-keys \
+    BuildDesc="qssi_64-user 16 BP2A.250605.015 1768189692380 release-keys" \
+    BuildFingerprint=OnePlus/CPH2573IN/OP595DL1:16/BP2A.250605.015/U.R4T3.248d671-27dbe8-27dbe5:user/release-keys \
     DeviceName=OP595DL1 \
     DeviceProduct=CPH2573 \
     SystemDevice=OP595DL1 \
